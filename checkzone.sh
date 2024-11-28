5cb7127eafe823531636c3e038bbd453012ead32
George-Opus <benjamin.navarro@septeo.com>
2024-11-28 17:41:41 +0100

    aaahhh

--- checkzone.sh
+++ checkzone.sh
@@ -2,11 +2,12 @@
 
 # Chemin du fichier de configuration
 CONFIG_FILE="/etc/named.*.zones"
-ll /etc/named.*.zones | awk '{print $9}' | while read -r file; do
+ll "$CONFIG_FILE" | awk '{print $9}' | while read -r file; do
+    echo "Vérification de la vue $file"
 # Extraire et vérifier chaque fichier de zone
-    grep '^zone.*{$' "$1" | awk '{print $2}' | while read -r zone; do
+    grep '^zone.*{$' "$file" | awk '{print $2}' | while read -r zone; do
         # Trouver le chemin du fichier de zone correspondant
-        zone_file=$(grep "file \"$zone" "$1" | awk -F'"' '{print$2}')
+        zone_file=$(grep "file \"$zone" "$file" | awk -F'"' '{print$2}')
         filename=/var/named/$zone_file
         if [ -f "$filename" ]; then
             #echo "Vérification de la zone $zone avec le fichier $zone_file..."
