20ca0bf44dbb105369efa5d2f4817cd9b6934194
George-Opus <benjamin.navarro@septeo.com>
2024-12-03 15:04:28 +0100

    beaucoup plus jolie

--- checkzone.sh
+++ checkzone.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-
-# Chemin du fichier de configuration
+RED='\033[31m'
+RESET='\033[0m'
+BLUE='\033[34m'
 CONFIG_FILE="/etc/named.*.zones"
-ll "$CONFIG_FILE" | awk '{print $9}' | while read -r file; do
-echo "Vérification de la vue $file"
-# Extraire et vérifier chaque fichier de zone
+ls -l $CONFIG_FILE | awk '{print $9}' | while read -r file; do
+	if [ "$1" = "-v" ] || [ "$1" = "-vv"  ]
+ 	then
+		echo -e  ${BLUE}"Vérification de la vue $file"${RESET}
+ 	fi
 	grep '^zone.*{$' "$file" | awk '{print $2}' | while read -r zone; do
-   	# Trouver le chemin du fichier de zone correspondant
         	zone_file=$(grep "file \"$zone" "$file" | awk -F'"' '{print$2}')
         	filename=/var/named/$zone_file
-        	if [ -f "$filename" ]; then
-            		#echo "Vérification de la zone $zone avec le fichier $zone_file..."
-            		check=$(named-checkzone "$zone" "$filename" | grep "OK")
-            		if [ -z "$check" ]; then
-                	named-checkzone "$zone" "$filename"
-            		fi
+        	if [ -f $filename ]
+		then
+			if [ "$1" = "-vv" ]
+			then
+				echo "Vérification de la zone $zone avec le fichier $zone_file..."
+			fi
+			check=$(named-checkzone "$zone" "$filename" | grep "OK")
+        		if [ -z $check ]
+			then
+				echo -e ${RED}| $(named-checkzone "$zone" "$filename")${RESET}
+			fi
         	fi
     	done
 done
